266e71677efd6d372b4b21c8b06928a3a420cbb3
zhoulidong <zhoulidong@nxin.com>
2020-07-26 23:31:20 +0800

    开始1

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -14,6 +14,7 @@ import java.io.IOException;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
+        //开始1
         JestClientFactory factory = new JestClientFactory();
 //增加测试2
         HttpClientConfig config = new HttpClientConfig.Builder("http://local1:9200").build();
